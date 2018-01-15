4e5dca7b67142286b5c0ba51f73130dc9527839a
Sergey Zobnin <sergei.s.zobnin@gmail.com>
2018-01-15 19:53:50 +0300

    Extended demo with blender-generated model

--- src/main/kotlin/LibKtxDemo.kt
+++ src/main/kotlin/LibKtxDemo.kt
@@ -1,3 +1,4 @@
+import com.badlogic.gdx.Files
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
@@ -9,7 +10,9 @@ import com.badlogic.gdx.graphics.g3d.*
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight
+import com.badlogic.gdx.graphics.g3d.loader.G3dModelLoader
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController
+import com.badlogic.gdx.utils.UBJsonReader
 import ktx.app.KtxApplicationAdapter
 
 fun main(args: Array<String>) {
@@ -33,10 +36,10 @@ class My3DTest : KtxApplicationAdapter {
     cam = PerspectiveCamera(67f,
         Gdx.graphics.getWidth().toFloat(),
         Gdx.graphics.getHeight().toFloat())
-    cam.translate(10f, 10f, 10f)
-    cam.lookAt(0f, 0f, 0f)
+    cam.translate(50f, -100f, 50f)
+    cam.lookAt(0f, -0f, -0f)
     cam.near = 1f;
-    cam.far = 300f
+    cam.far = 800f
     cam.update()
 
     val box = ModelBuilder().createBox(5f, 5f, 5f,
@@ -45,11 +48,17 @@ class My3DTest : KtxApplicationAdapter {
     models.add(box)
     instances.add(ModelInstance(box))
 
+
     val cyl = ModelBuilder().createCylinder(5f, 20f, 5f, 10,
         Material(ColorAttribute.createDiffuse(Color.GREEN)),
         (Position or Normal).toLong())
     models += cyl
     instances += ModelInstance(cyl)
+
+    val g3dModelLoader = G3dModelLoader(UBJsonReader())
+    val untitled = g3dModelLoader.loadModel(Gdx.files.getFileHandle("untitled.g3db", Files.FileType.Internal))
+    models += untitled
+    instances += ModelInstance(untitled)
 
     environment = Environment()
     environment.set(ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f))
@@ -66,6 +75,7 @@ class My3DTest : KtxApplicationAdapter {
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT or GL20.GL_DEPTH_BUFFER_BIT)
 
     modelBatch.begin(cam)
+    println(cam.direction)
     modelBatch.render(instances, environment)
     modelBatch.end()
   }
