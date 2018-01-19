2b76f53d1be4ee67f4fcddef51cfe7ba97c2e371
Sergey Zobnin z0036pky <sergey.zobnin@siemens.com>
2018-01-19 09:42:43 +0300

    Uodated model (export from sketchup stl -> blender fbx -> g3db)

--- src/main/kotlin/LibKtxDemo.kt
+++ src/main/kotlin/LibKtxDemo.kt
@@ -1,3 +1,4 @@
+import com.badlogic.gdx.Files
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
@@ -10,10 +11,12 @@ import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute
 import com.badlogic.gdx.graphics.g3d.attributes.TextureAttribute
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight
+import com.badlogic.gdx.graphics.g3d.loader.G3dModelLoader
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController
 import com.badlogic.gdx.math.Matrix4
 import com.badlogic.gdx.math.Quaternion
 import com.badlogic.gdx.math.Vector3
+import com.badlogic.gdx.utils.UBJsonReader
 import ktx.app.KtxApplicationAdapter
 import ktx.math.vec3
 
@@ -29,6 +32,7 @@ class My3DTest : KtxApplicationAdapter {
   lateinit var modelBatch: ModelBatch
   lateinit var environment: Environment
   lateinit var cameraInputController: CameraInputController
+  lateinit var nikishInstance: ModelInstance
   val models: MutableList<Model> = mutableListOf()
   val instances: MutableList<ModelInstance> = mutableListOf()
 
@@ -38,38 +42,29 @@ class My3DTest : KtxApplicationAdapter {
     cam = PerspectiveCamera(67f,
         Gdx.graphics.getWidth().toFloat(),
         Gdx.graphics.getHeight().toFloat())
-    cam.translate(1.4f, 1.4f, 1.4f)
+    cam.translate(10f, 10f, 10f)
     cam.lookAt(0f, 0f, 0f)
     cam.near = 1f;
-    cam.far = 10f
+    cam.far = 100f
     cam.update()
-
-    //    val box = ModelBuilder().createBox(5f, 5f, 5f,
-    //        Material(ColorAttribute.createDiffuse(Color.GREEN)),
-    //        (Position or Normal).toLong())
-    //    models.add(box)
-    //    instances.add(ModelInstance(box))
 
     val texture = Texture(Gdx.files.internal("nikish.png"))
     val material = Material(TextureAttribute.createDiffuse(texture))
     val attr = (Position or Normal or TextureCoordinates).toLong()
 
-    val mod = ModelBuilder()
-        .createCone(2f, 2f, 2f, 100,
+    val nikish = ModelBuilder()
+        .createSphere(3f, 3f, 3f, 100, 100,
             material, attr)
-    models += mod
-    instances += ModelInstance(mod)
+    models += nikish
+    nikishInstance = ModelInstance(nikish)
+    instances += nikishInstance
 
-    //    val cyl = ModelBuilder().createCylinder(5f, 20f, 5f, 10,
-    //        Material(ColorAttribute.createDiffuse(Color.GREEN)),
-    //        (Position or Normal).toLong())
-    //    models += cyl
-    //    instances += ModelInstance(cyl)
-    //
-    //    val g3dModelLoader = G3dModelLoader(UBJsonReader())
-    //    val untitled = g3dModelLoader.loadModel(Gdx.files.getFileHandle("untitled.g3db", Files.FileType.Internal))
-    //    models += untitled
-    //    instances += ModelInstance(untitled)
+    val g3dModelLoader = G3dModelLoader(UBJsonReader())
+    val room = g3dModelLoader
+        .loadModel(
+            Gdx.files.getFileHandle("room_prototype.g3db", Files.FileType.Internal))
+    models += room
+    instances += ModelInstance(room)
 
     environment = Environment()
     environment.set(ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f))
@@ -92,12 +87,9 @@ class My3DTest : KtxApplicationAdapter {
     Gdx.gl.glViewport(0, 0, Gdx.graphics.width, Gdx.graphics.height)
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT or GL20.GL_DEPTH_BUFFER_BIT)
 
-
-    instances.forEach {
-      it.transform = Matrix4(vec3(0f, 0f, 0f),
-          Quaternion(rotAngle1, angle1++),
-          Vector3(1.0f, 1.0f, 1.0f))
-    }
+    nikishInstance.transform = Matrix4(vec3(0f, 0f, 0f),
+        Quaternion(rotAngle1, angle1++),
+        Vector3(1.0f, 1.0f, 1.0f))
 
     modelBatch.begin(cam)
     modelBatch.render(instances, environment)
